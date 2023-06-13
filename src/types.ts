906fb0a45c435faabfa3ee9f089387a2895c825b
Iker Garcia Ramirez <ikergarciadev@gmail.com>
2023-06-13 20:17:20 +0200

    feat(App): 🏷️ add required types

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,16 @@
+import { DomainTuple } from 'victory';
+
 export interface Measure {
   time: number;
   value: number;
 }
 
 export type Domain = [number, number];
+
+export type AxisDomain = { x: DomainTuple; y: DomainTuple };
+
+export interface InteractiveGraphic {
+  domain: Domain;
+  data: Measure[];
+  handleZoom: (domain: AxisDomain) => void;
+}
