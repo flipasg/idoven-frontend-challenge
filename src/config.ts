fe4c0c5a4f459eb29aa8a0676894f96ddfba466f
Iker Garcia Ramirez <ikergarciadev@gmail.com>
2023-06-13 20:49:06 +0200

    feat(App): :bento: add sample data

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,5 @@ import { Domain } from './types';
 export const DEFAULT_DOMAIN: Domain = [0, 5]
 export const DOMAIN_STEP = 5;
 
+// TODO: should change to a real file (now contains a sample data)
 export const DATA_URL = '/idoven-frontend-challenge/assets/data/14-29-05_data_data.txt';
