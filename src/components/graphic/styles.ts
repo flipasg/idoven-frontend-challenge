f975db7cc007a6fa4081491b3baf3595c11c91d5
Iker Garcia Ramirez <ikergarciadev@gmail.com>
2023-06-14 10:36:09 +0200

    feat(Graphich): ♻️ add colors to graphic theme

--- src/components/graphic/styles.ts
+++ src/components/graphic/styles.ts
@@ -1,14 +1,15 @@
 import { Grid, styled } from '@mui/material'
+import { GREEN, LIGHT_GRAY } from './theme'
 
 export const LINE_STYLES = {
-  data: { stroke: '#959427', strokeWidth: 0.7, opacity: 1 },
+  data: { stroke: GREEN, strokeWidth: 0.7, opacity: 1 },
 }
 
-export const BRUSH_STYLES = { fill: '#707070', fillOpacity: 0.1 }
+export const BRUSH_STYLES = { fill: LIGHT_GRAY, fillOpacity: 0.1 }
 
 // Used styled-components to style the graphic container
 // instead of using Material UI's Grid component:
-// <Grid container direction='column'justifyContent='center' alignItems='center' height='100%' width='100%'>
+// <Grid container direction='column'height='100%' width='100%'>
 export const GraphicContainer = styled(Grid)`
   display: flex;
   flex-direction: column;
--- src/components/graphic/theme.ts
+++ src/components/graphic/theme.ts
@@ -2,6 +2,8 @@ import { VictoryTheme } from 'victory';
 import deepmerge from 'deepmerge';
 
 const GRAY = '#5e5e5d';
+export const LIGHT_GRAY = '#707070';
+export const GREEN = '#959427';
 const chartTheme = {
   axis: {
     style: {
